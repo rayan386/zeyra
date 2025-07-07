1a75d64e4dbb33846e01f10bfe7f090f666fe864
globex <onlydanzo87@gmail.com>
2025-07-07 16:04:52 +0200

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src//*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6B00',
+        secondary: '#2E2E2E',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
