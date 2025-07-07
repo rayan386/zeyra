12f0c9f1021a6f60eafa5a4db551524c8eb7d0c7
globex <onlydanzo87@gmail.com>
2025-07-07 15:28:16 +0200

    add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "zeyara-global.firebaseapp.com",
+  projectId: "zeyara-global",
+  storageBucket: "zeyara-global.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
